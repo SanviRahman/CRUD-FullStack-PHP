b8453f392d04612064d2a6fe126c270384f51b7e
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-10-07 13:05:19 +0600

    Done

--- delete.php
+++ delete.php
@@ -5,6 +5,12 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // Method Check 
 if ($_SERVER['REQUEST_METHOD'] !== 'DELETE') {
     http_response_code(405); // Method Not Allowed
